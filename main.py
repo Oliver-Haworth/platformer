ad83765b02dd3ea686cb4ff7a58c815fe4102575
Oliver-Haworth <oliverphaworth@gmail.com>
2025-12-18 19:37:27 +0000

    resolved issue after resolving case sensitivity

---
+++ main.py
@@ -0,0 +1,100 @@
+# --- MAIN.PY ---
+
+
+
+
+
+#Import Library
+
+
+import os
+
+
+import pygame
+
+
+from settings import *
+
+
+from tilemap import Tilemap
+
+
+
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+
+
+
+level_path = os.path.join(BASE_DIR, "levels", "level1.txt")
+
+
+dirt_path = os.path.join(BASE_DIR, "Assets", "Dirt.png")
+
+
+
+
+
+#Pygame Setup
+
+
+pygame.init()
+
+
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+
+clock = pygame.time.Clock()
+
+
+pygame.display.set_caption(WINDOW_TITLE)
+
+
+level = Tilemap(level_path, dirt_path, 16)
+
+
+
+
+
+running = True
+
+
+while running:
+
+
+    for event in pygame.event.get():
+
+
+        if event.type == pygame.QUIT:
+
+
+            running = False
+
+
+
+
+
+    screen.fill((30, 30, 30))
+
+
+
+
+
+    level.draw(screen)
+
+
+
+
+
+    pygame.display.flip()
+
+
+    clock.tick(60)
+
+
+
+
+
+pygame.quit()
